9a4320ca6e564af3c26ccab772fb1db6d9a11103
abhishektiwari283 <142007480+Code-writter@users.noreply.github.com>
2025-06-27 18:42:14 +0530

    Update Logo, Refine Theme Toggle, and Add GitHub Star Button #189

--- src/components/_components/header.tsx
+++ src/components/_components/header.tsx
@@ -1,10 +1,12 @@
 import {Link} from 'react-router-dom'
 // import { Logo } from '@/components/logo'
-import { Menu, Moon, Sun, Laptop, X } from 'lucide-react'
-import { Button } from '@/components/ui/button'
+import { Menu, Moon, Sun, X } from 'lucide-react'
 import React from 'react'
 import { cn } from '@/lib/utils'
 import { useTheme } from "@/components/theme-provider"
+import { GitHubStarsButton } from '../CustomComps/buttons/github-stars'
+import logg from '../rdk.svg';
+import loggd from '../rdkd.svg';
 
 
 const menuItems = [
@@ -17,6 +19,7 @@ const menuItems = [
 
 export const Header = () => {
     const { theme, setTheme } = useTheme()
+    const isDark = theme === "dark"
     const [menuState, setMenuState] = React.useState(false)
     const [isScrolled, setIsScrolled] = React.useState(false)
 
@@ -40,7 +43,18 @@ export const Header = () => {
                                 aria-label="home"
                                 className="flex items-center space-x-2">
                                 {/* <Logo /> */}
-                                <h1 className=' dark:text-white hover:text-white duration-150 ease-in-out hover:cursor-pointer' >README DESIGN KIT</h1>
+                                <img 
+                                  src={loggd} 
+                                  alt="README Design Kit - Light Logo" 
+                                  className="h-8 object-contain block dark:hidden"
+                                />
+                                 {/* Dark Mode Logo */}
+                                <img 
+                                  src={logg} 
+                                  alt="README Design Kit - Dark Logo" 
+                                  className="h-8 object-contain hidden dark:block"
+                                />
+                                {/* <h1 className=' dark:text-white hover:text-white duration-150 ease-in-out hover:cursor-pointer' >README DESIGN KIT</h1> */}
                             </Link>
 
                             <button
@@ -53,7 +67,7 @@ export const Header = () => {
                         </div>
 
                         <div className="absolute inset-0 m-auto hidden size-fit lg:block">
-                            <ul className="flex gap-8 text-sm">
+                            <ul className={cn("flex text-sm transition-all duration-300 items-center", isScrolled ? "gap-6" : "gap-8")}>
                                 {menuItems.map((item, index) => (
                                     <li key={index}>
                                         <Link
@@ -66,7 +80,7 @@ export const Header = () => {
                             </ul>
                         </div>
 
-                        <div className="bg-background in-data-[state=active]:block lg:in-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
+                        <div className={cn("bg-background in-data-[state=active]:block lg:in-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent transition-all duration-300", isScrolled ? "space-y-4 lg:gap-4" : "space-y-8 lg:gap-8")}>
                             <div className="lg:hidden">
                                 <ul className="space-y-6 text-base">
                                     {menuItems.map((item, index) => (
@@ -80,34 +94,25 @@ export const Header = () => {
                                     ))}
                                 </ul>
                             </div>
-                            <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
-                                <div className={cn(isScrolled && 'lg:hidden', 'inline-flex items-center rounded-md  border-border backdrop-blur-lg overflow-hidden')}> 
-                                    <Button variant={theme==='light' ? 'default' : 'ghost'} size="sm" aria-label="Light mode" onClick={() => setTheme('light')} className={cn('h-8 w-8 transition-opacity', theme==='light' ? 'bg-primary text-primary-foreground shadow-sm' : 'opacity-75 hover:opacity-100')}>
-                                        <Sun className="h-5 w-5" />
-                                    </Button>
-                                    <Button variant={theme==='dark' ? 'default' : 'ghost'} size="sm" aria-label="Dark mode" onClick={() => setTheme('dark')} className={cn('h-8 w-8 transition-opacity', theme==='dark' ? 'bg-primary text-primary-foreground shadow-sm' : 'opacity-75 hover:opacity-100')}>
-                                        <Moon className="h-5 w-5" />
-                                    </Button>
-                                    <Button variant={theme==='system' ? 'default' : 'ghost'} size="sm" aria-label="System mode" onClick={() => setTheme('system')} className={cn('h-8 w-8 transition-opacity', theme==='system' ? 'bg-primary text-primary-foreground shadow-sm' : 'opacity-75 hover:opacity-100')}>
-                                        <Laptop className="h-5 w-5" />
-                                    </Button>
+                            <div className={cn("flex w-full flex-col sm:flex-row md:w-fit transition-all duration-300 items-center", isScrolled ? "space-y-2 sm:gap-3" : "space-y-3 sm:gap-4")}>
+                                {/* <div className={cn(isScrolled && 'lg:hidden', 'inline-flex items-center rounded-md  border-border backdrop-blur-lg overflow-hidden')}>  */}
+                                <div className="flex items-center justify-center h-10 w-10 -my-1">
+                                    <button 
+                                        onClick={() => setTheme(isDark ? "light" : "dark")} 
+                                        className={`p-2 rounded-full flex items-center justify-center hover:bg-accent/50 transition-colors
+                                            ${isDark ? "rotate-180" : "rotate-0"}
+                                        `}
+                                        aria-label="Toggle theme"
+                                    >
+                                        {isDark ? (
+                                            <Sun className="h-5 w-5 text-yellow-500" />
+                                        ) : (
+                                            <Moon className="h-5 w-5 text-blue-500" />
+                                        )}
+                                    </button>
                                 </div>
-                                <Button
-                                    asChild
-                                    size="sm"
-                                    className={cn(isScrolled && 'lg:hidden')}>
-                                    <Link to="/elements">
-                                        <span>Get Started</span>
-                                    </Link>
-                                </Button>
-                                <Button
-                                    asChild
-                                    size="sm"
-                                    className={cn(isScrolled ? 'lg:inline-flex' : 'hidden')}>
-                                    <Link to="/elements">
-                                        <span>Get Started</span>
-                                    </Link>
-                                </Button>
+                                {/* </div> */}
+                                <GitHubStarsButton username="Mayur-Pagote" repo="README_Design_Kit" />
                             </div>
                         </div>
                     </div>
