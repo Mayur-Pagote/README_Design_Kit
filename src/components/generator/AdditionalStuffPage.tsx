ced3441f44fc9a851b7363def816be93fe660ea6
abhishektiwari283 <142007480+Code-writter@users.noreply.github.com>
2025-07-03 13:46:24 +0530

    Initialized Additional and Final  page

---
+++ src/components/generator/AdditionalStuffPage.tsx
@@ -0,0 +1,7 @@
+export default function AdditionalStuffPage() {
+    return (
+        <div>
+            <h1>Additional Stuff Page</h1>
+        </div>
+    );
+}
---
+++ src/components/generator/FinalPage.tsx
@@ -0,0 +1,7 @@
+export default function FinalPage() {
+    return (
+        <div>
+            <h1>Final Page</h1>
+        </div>
+    );
+}
