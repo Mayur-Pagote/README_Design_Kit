c558acb4e137e4b108484ec8902ada9165582d0f
Anvesha Srivastava <207409304+Anvesha-Sri@users.noreply.github.com>
2026-06-13 22:30:15 +0530

    Add TypingSvgGeneratorPage component

---
+++ src/pages/TypingSvgGeneratorPage.tsx
@@ -0,0 +1,7 @@
+import { TypingSvgGenerator } from "@/components/generator/TypingSvgGenerator";
+
+export default function TypingSvgGeneratorPage() {
+  return (
+    <TypingSvgGenerator />
+  );
+}
